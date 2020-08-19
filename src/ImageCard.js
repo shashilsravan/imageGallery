39d6ecb687d2636ddc97ccada116f2fcbfa16e93
shashilsravan <55411018+shashilsravan@users.noreply.github.com>
2020-08-19 12:26:51 +0530

    Update ImageCard.js

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -24,7 +24,7 @@ function ImageCard({ image }) {
                 <strong>Likes: </strong> {image.likes}
                 </li>
             </ul>
-            <a className="bg-transaparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mt-20 border border-blue-500 hover:border-transparent rounded-full" href={image.pageURL}>Download Image</a>
+            <a target="_blank" className="bg-transaparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mt-20 border border-blue-500 hover:border-transparent rounded-full" href={image.pageURL}>Download Image</a>
             </div>
             <div className="px-6 py-4">
             {tags.map((tag, index) => <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2">
