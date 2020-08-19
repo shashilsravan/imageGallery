471026deacedcf5de75a1c1fc259b4923ec24f55
sravan <shashilsravan.ss.ss@gmail.com>
2020-08-19 11:02:35 +0530

    Updates

---
+++ precache-manifest.efdbce5d0b7d2db3f1100b541bedb1c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5cdf66b1380b809e4f5ab229423e34",
+    "url": "/imageGallery/index.html"
+  },
+  {
+    "revision": "8e3be389861b2db161c3",
+    "url": "/imageGallery/static/css/main.068b2e3a.chunk.css"
+  },
+  {
+    "revision": "1325ce2b592a10590812",
+    "url": "/imageGallery/static/js/2.82a47e3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imageGallery/static/js/2.82a47e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3be389861b2db161c3",
+    "url": "/imageGallery/static/js/main.ccb9d019.chunk.js"
+  },
+  {
+    "revision": "aff09b529e1c10eff8ff",
+    "url": "/imageGallery/static/js/runtime-main.77a97d29.js"
+  }
+]);
